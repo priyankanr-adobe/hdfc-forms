352f6e23f546bcc4d18b5a5121dd5ab8ff5d5193
priyankanr <pnr@adobe.com>
2026-04-30 11:59:42 +0530

    ui

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -306,13 +306,25 @@ function readRangeValue(fieldName) {
 
 function updateSliderUI(fieldName, value) {
   const wrapper = document.querySelector(
-    `.field-${fieldName} .range-widget-wrapper, 
-     .field-${fieldName.replaceAll("_","-")} .range-widget-wrapper`
+    `.field-${fieldName} .range-widget-wrapper, .field-${fieldName.replaceAll("_", "-")} .range-widget-wrapper`
   );
 
-  if (wrapper) {
-    wrapper.style.setProperty("--current-steps", value);
-  }
+  const input = document.querySelector(
+    `.field-${fieldName} input[type="range"], .field-${fieldName.replaceAll("_", "-")} input[type="range"]`
+  );
+
+  if (!wrapper || !input) return;
+
+  input.value = value;
+  input.setAttribute("value", value);
+
+  const min = Number(input.min || 0);
+  const currentSteps = Number(value) - min;
+
+  wrapper.style.setProperty("--current-steps", currentSteps);
+
+  input.dispatchEvent(new Event("input", { bubbles: true }));
+  input.dispatchEvent(new Event("change", { bubbles: true }));
 }
 
 function calculateLoanOffer(globals) {
